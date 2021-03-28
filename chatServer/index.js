6901b35ced10ef1c741b0c707242596f1ac22b41
Ruba Hashash <ruba.hashash@gmail.com>
2021-03-28 13:52:09 +0300

    use sockets in backend and frontend

--- chatServer/index.js
+++ chatServer/index.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+var users = []
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,8 +13,18 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', (data) => {
+      let index = users.findIndex(user =>{
+        return user.userId === data.userId
+      })
+        users[index].socket.emit('chat message', data.message);
+      });
+      socket.on('online', (userId) => {
+        users.push({
+          socket,
+          userId
+        })
+        console.log(users);
       });
   });
 
